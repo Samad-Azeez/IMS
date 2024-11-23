40a043c1e119bbd4ce9c611aa7afde36380f321c
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-23 14:02:37 +0100

    feat: Implement custom error handling middleware

--- Server/middleware/error-handler.js
+++ Server/middleware/error-handler.js
@@ -1,8 +1,32 @@
-export const errorHandlerMiddleware = async (err, req, res, next) => {
-  console.error(err); // Logs the error for debugging purposes
+// import { CustomAPIError } from '../errors/custom-api.js';
+import { StatusCodes } from 'http-status-codes';
 
-  // Sending back the error message and a generic response
-  return res.status(500).json({
-    message: err.message || 'Something went wrong, please try again later.',
-  });
+export const errorHandlerMiddleware = (err, req, res, next) => {
+  // not needed but added for clarity to also output the error to the console 😛😛
+  console.error(err);
+
+  // set default error object with default values if not provided by the error object
+  let customError = {
+    //set default
+    StatusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'something went wrong try again later',
+  };
+
+  // handle custom API error
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  // handle mongoose duplicate key error
+  if (err.code && err.code === 11000) {
+    customError = {
+      StatusCode: 400,
+      msg: `Duplicate value entered for ${
+        Object.keys(err.keyValue)[0]
+      } field, please choose another value`,
+    };
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.StatusCode).json({ msg: customError.msg });
 };
